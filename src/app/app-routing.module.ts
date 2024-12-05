c52ae821918489f2ca9e5f4bd2d3bb8e23671d9d
sagara <sahire@1gen.io>
2024-12-05 13:28:40 +0530

    set-up all project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EmailComponent } from './components/email/email.component';
+import { MovieComponent } from './movie_service_components/movie/movie.component';
+import { MovieListComponent } from './movie_service_components/movie-list/movie-list.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  component:HomeComponent,
+  path:"",
+  pathMatch:"full"
+},
+{
+  component:EmailComponent,
+  path: "sendemail",
+  pathMatch:"full"
+
+},
+{
+  component:MovieComponent,
+  path:"gotoMovie",
+  pathMatch:"full"
+
+},{
+  component:MovieListComponent,
+  path:"movieList",
+  pathMatch:"full"
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,50 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MovieComponent } from './movie_service_components/movie/movie.component';
+import { EmailComponent } from './components/email/email.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MovieListComponent } from './movie_service_components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MovieComponent,
+    EmailComponent,
+    HomeComponent,
+    MovieListComponent
+    
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
